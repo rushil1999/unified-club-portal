9326a9203c774e3f565df406136570703ae956d0
rushil1999 <shahrushil1999@gmail.com>
2021-07-12 16:24:11 +0530

    [ADD] Client

---
+++ client/unified-club-portal/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import SignUp from './components/SignUp';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path='/signup' exact component={SignUp}></Route>
+        </Switch>
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
+
+
+/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */
