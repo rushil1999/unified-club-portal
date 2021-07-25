872831a17d98dfc43a2b2cda5321562b6829fa01
Harshiv Patel <pharshiv10@gmail.com>
2021-07-25 20:52:43 +0530

    [UPD] added event status chip

--- client/unified-club-portal/src/components/event/EventInfo.js
+++ client/unified-club-portal/src/components/event/EventInfo.js
@@ -2,7 +2,9 @@ import React from "react";
 import CardContent from "@material-ui/core/CardContent";
 import Card from "@material-ui/core/Card";
 import Typography from "@material-ui/core/Typography";
+import Chip from "@material-ui/core/Chip";
 import { makeStyles } from "@material-ui/core/styles";
+import { getEventStatus } from "../../services/eventServices";
 
 const useStyles = makeStyles({
   root: {
@@ -20,13 +22,27 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 12,
   },
+  chip_start: {
+    backgroundColor: "green",
+    marginBottom: "10px",
+  },
+  chip_end: {
+    backgroundColor: "red",
+    marginBottom: "10px",
+  },
 });
 
 const EventInfo = props => {
   const classes = useStyles();
   const { event } = props;
 
-  const { name, desc, capacity, from, to, status } = event;
+  const { name, desc, capacity, from, to } = event;
+  const status = getEventStatus(from, to);
+  var statusText = "UPCOMING",
+    statusClass = status === 2 ? classes.chip_end : classes.chip_start;
+
+  if (status === 1) statusText = "ON-GOING";
+  else if (status === 2) statusText = "ENDED";
   const startDate = new Date(from);
   const startDateString = `${startDate.getDate()}/${startDate.getMonth()}/${startDate.getFullYear()}   ${startDate.getHours()}:${startDate.getMinutes()}`;
 
@@ -64,9 +80,12 @@ const EventInfo = props => {
               <b>{toDateString}</b>
             </i>
           </Typography>
-          <Typography variant="h6" style={{ color: "darkslategrey" }}>
-            {status}
-          </Typography>
+          <Chip
+              color="secondary"
+              size="small"
+              label={statusText}
+              className={statusClass}
+            />
         </CardContent>
       </Card>
     </React.Fragment>
