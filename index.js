829ff3a83dc6161d3a82f68fc6ec164b2c2c8480
Harshiv Patel <pharshiv10@gmail.com>
2021-07-27 14:20:54 +0530

    [UPD] Fixed port for server from 3000 to 5000

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ if (result.error) {
 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const CONNECTION_URL = process.env.MONGODB_URI;
 const connectionParams = {
   useNewUrlParser: true,
